971cca5fb115040433feb13b0e5b1bbbfbc40785
amesanch <developer.geek.sb@gmail.com>
2025-07-01 14:48:08 +0200

    primera subida proyecto

---
+++ src/test/java/com/asb/housingplatform/AsbHousingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asb.housingplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsbHousingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
